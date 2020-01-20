42fa7c84958787715c6ff1a7ec65cc93fb8941db
benhannel <ben@bensprojects.com>
2020-01-19 23:43:36 -0800

    Specify GCS path for videos and subs as CLI arguments. Fix bug in IA search matching. Print on error messages while downloading from IA.

--- ia_download.py
+++ ia_download.py
@@ -9,17 +9,17 @@ from subprocess import check_output, check_call
 from tqdm import tqdm
 
 
-GCS_VIDEO_PATH = 'gs://esper/tvnews/videos'
-GCS_VIDEO_OUTPUT_PATH = 'gs://esper/tvnews/videos'
-
-
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-y', dest='year', type=int, required=True)
+    parser.add_argument('-y', dest='year', type=int, required=True, help='The year for which to download videos.')
     parser.add_argument('--subs', dest='subs_dir', type=str, default='subs',
                         help='Directory to save captions to')
     parser.add_argument('--list', dest='list_file', type=str,
                         help='File to write the list of downloaded videos')
+    parser.add_argument('--gcs-video-path', dest='gcs_video_path', type=str, default='gs://esper/tvnews/videos',
+                        help='The path in Google cloud to which videos should be uploaded.')
+    parser.add_argument('--gcs-caption-path', dest='gcs_caption_path', type=str, default=None,
+                        help='The path in Google cloud to which captions/subtitles should be uploaded. If not specified, they are not uploaded')
     return parser.parse_args()
 
 
@@ -28,10 +28,10 @@ def parse_ia_identifier(s):
     return os.path.splitext(s.split('/')[-1])[0]
 
 
-def list_downloaded_videos(year):
+def list_downloaded_videos(year, gcs_video_path):
     """List the videos in the bucket"""
     output = check_output(
-        ['gsutil', 'ls', '{}/*{}*'.format(GCS_VIDEO_PATH, year)]).decode()
+        ['gsutil', 'ls', '{}/*_{}*'.format(gcs_video_path, year)]).decode()
     videos = [x for x in output.split('\n') if x.strip()]
     return {parse_ia_identifier(x) for x in videos}
 
@@ -41,43 +41,47 @@ def list_ia_videos(year):
     identifiers = []
     identifier_re = re.compile(r'^[A-Z]+_[0-9]{8}_', re.IGNORECASE)
     for p in prefixes:
-        output = check_output(
-            ['ia', 'search', '{}_{}'.format(p, year)]).decode()
+        query_string = '{}_{}'.format(p, year)
+        output = check_output(['ia', 'search', query_string]).decode()
         for line in output.split('\n'):
             line = line.strip()
             if line:
                 identifier = json.loads(line)['identifier']
-                if identifier.startswith(p) and identifier_re.match(identifier):
+                if identifier.startswith(query_string):
                     identifiers.append(identifier)
     return identifiers
 
 
-def download_video_and_subs(identifier, subs_dir):
+def download_video_and_subs(identifier, subs_dir, gcs_video_path, gcs_caption_path):
     try:
         check_call(['ia', 'download', '--glob=*.mp4', identifier])
         check_call(['ia', 'download', '--glob=*.srt', identifier])
-    except:
-        pass
+    except Exception as e:
+        print("Error while downloading from internet archive", e)
+
     if os.path.exists(identifier):
         for fname in os.listdir(identifier):
             if fname.endswith('.mp4'):
                 local_path = os.path.join(identifier, fname)
-                cloud_path = GCS_VIDEO_OUTPUT_PATH + '/' + fname
+                cloud_path = gcs_video_path + '/' + fname
                 check_call(['gsutil', 'cp', '-n', local_path, cloud_path])
             if fname.endswith('.srt'):
                 local_path = os.path.join(identifier, fname)
                 subs_path = os.path.join(subs_dir, fname)
                 shutil.copyfile(local_path, subs_path)
+                if gcs_caption_path is not None:
+                    gcs_subs_path = os.path.join(gcs_caption_path, fname)
+                    check_call(['gsutil', 'cp', '-n', local_path, gcs_subs_path])
         # FIXME: probably want to keep the video files around locally
         shutil.rmtree(identifier)
 
 
-def main(year, subs_dir, list_file):
+def main(year, subs_dir, list_file, gcs_video_path, gcs_caption_path):
     if not os.path.exists(subs_dir):
         os.makedirs(subs_dir)
 
     print('Listing downloaded videos')
-    downloaded = list_downloaded_videos(year)
+    downloaded = list_downloaded_videos(year, gcs_video_path)
 
     print('Listing available videos')
     available = list_ia_videos(year)
@@ -91,7 +95,7 @@ def main(year, subs_dir, list_file):
 
     print('Downloading')
     for identifier in tqdm(to_download):
-        download_video_and_subs(identifier, subs_dir)
+        download_video_and_subs(identifier, subs_dir, gcs_video_path, gcs_caption_path)
 
 
 if __name__ == '__main__':
