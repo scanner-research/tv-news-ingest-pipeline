16127e105171f89c4f79686b0cbb6db7ae71e5b5
Jeremy Ephron <jeremyephron@gmail.com>
2020-03-07 00:05:35 +0000

    update deps

--- install_deps.sh
+++ install_deps.sh
@@ -5,7 +5,7 @@ set -e  # exit on error
 git submodule init
 git submodule update
 
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
 # Install gentle (need to run install as root)
 (cd deps/gentle && sudo bash ./install.sh)
