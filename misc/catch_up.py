be594b1d748d0615b2e435c3f2a37fea5b3f5549
Jeremy Ephron <jeremyephron@gmail.com>
2020-03-20 08:26:43 +0000

    util import

--- misc/catch_up.py
+++ misc/catch_up.py
@@ -19,7 +19,31 @@ import shutil
 import subprocess
 import time
 
-from util.consts import ALL_OUTPUTS
+FILE_BBOXES = 'bboxes.json'
+FILE_EMBEDS = 'embeddings.json'
+FILE_BLACK_FRAMES = 'black_frames.json'
+FILE_COMMERCIALS = 'commercials.json'
+FILE_METADATA = 'metadata.json'
+FILE_GENDERS = 'genders.json'
+FILE_IDENTITIES = 'identities.json'
+FILE_IDENTITIES_PROP = 'identities_propogated.json'
+FILE_CAPTIONS = 'captions.srt'
+FILE_CAPTIONS_ORIG = 'captions_orig.srt'
+FILE_ALIGNMENT_STATS = 'alignment_stats.json'
+DIR_CROPS = 'crops'
+
+ALL_OUTPUTS = [
+    FILE_BBOXES,
+    FILE_EMBEDS,
+    FILE_METADATA,
+    FILE_GENDERS,
+    FILE_IDENTITIES,
+    FILE_IDENTITIES_PROP,
+    FILE_CAPTIONS,
+    FILE_CAPTIONS_ORIG,
+    FILE_ALIGNMENT_STATS,
+    DIR_CROPS
+]
 
 WORKING_DIR = '.catch_up_tmp'
 DOWNLOAD_DIR = os.path.join(WORKING_DIR, 'downloads')
